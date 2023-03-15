c73660a305987b3205966e0631b48c95acb5bdc2
Thayane Almeida <thayane.almeida196@gmail.com>
2023-03-15 13:39:26 -0300

    Criação da model, repository e controller

---
+++ BlogPessoal/blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,55 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "tb_postagens")
+public class Postagem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo titulo é Obrigatório!")
+	@Size(min = 5, max = 100, message = "O atributo titulo deve conter no minímo 05 e no máximo 100 caracteres")
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	
+
+}
